2747d0eed059cff5c10225d783cf4e33789fbff1
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2022-12-18 19:14:45 +0700

    add Task 15

---
+++ Task15/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+Console.WriteLine("Задача 15 ");
+
+Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek(int dayNumber)
+{
+    if (dayNumber == 6 || dayNumber == 7)
+    {
+        Console.WriteLine("(Этот день выходной) -> да");
+    }
+    else { Console.WriteLine("(Этот день не выходной) -> нет"); }
+}
+
+CheckingTheDayOfTheWeek(dayNumber);
+// void можно использовать в качестве возвращаемого типа void (или локальной функции) для определения того, что метод не возвращает значение. 
