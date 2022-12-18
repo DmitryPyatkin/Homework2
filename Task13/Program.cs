90cd2230756a3df472ee0a6179cbfb35c2364166
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2022-12-18 19:14:10 +0700

    add Task 13

---
+++ Task13/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.Clear();
+Console.WriteLine("Задача 13 ");
+
+Console.Write("Введи число: ");
+int inputValue  = Convert.ToInt32(Console.ReadLine());
+string stringValue = Convert.ToString(inputValue);
+if (stringValue.Length > 2){
+  Console.WriteLine("Третья цифра -> " + stringValue[2]);
+}
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
