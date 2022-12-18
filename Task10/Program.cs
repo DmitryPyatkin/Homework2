c6b9b0e0f0e3ee378e4ae40d9a6194920073e109
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2022-12-18 19:13:43 +0700

    add Task 10

---
+++ Task10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+System.Console.WriteLine("Задача 10");
+
+System.Console.Write("Введите трёхзначное число: ");
+int inputValue = Convert.ToInt32(Console.ReadLine()); // Приобразование стороки в число.
+// Метод ReadLine() используется для ввода строки символов типа string. Для преобразования в другой тип, нужно использовать метод Parse() или методы класса Convert.
+string stringValue = Convert.ToString(inputValue);
+Console.WriteLine("Вторая цифра этого числа -> " + stringValue[1]);
+// Метод ToString() приобразует число в стороку.
+
+
